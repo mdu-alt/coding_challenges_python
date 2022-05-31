78d2335db101b645c697c0e8e6bf48cff3aad1a8
Matthieu DUFOUR <matthieu.dufour@pm.me>
2022-06-01 00:11:59 +0200

    Add problem "PalindromeNumber"

---
+++ problems/palindrome_number.py
@@ -0,0 +1,31 @@
+class PalindromeNumber:
+    """
+    Given an integer n, return True if n is a palindrome integer.
+
+    Constraints:
+    * -2e31 <= n <= 2e31 - 1
+
+    Reference: https://leetcode.com/problems/palindrome-number/
+    """
+
+    @staticmethod
+    def is_palindrome_using_string(n: int) -> bool:
+        if n < 0 or (n > 0 and n % 10 == 0):
+            return False
+
+        s = str(n)
+
+        return s == s[::-1]
+
+    @staticmethod
+    def is_palindrome_using_arithmetic(n: int) -> bool:
+        if n < 0 or (n > 0 and n % 10 == 0):
+            return False
+
+        output = 0
+
+        while n > output:
+            output = output * 10 + n % 10
+            n //= 10
+
+        return n == output or n == output // 10
